7ad5224e2f15a40bbecee52f11ad67c316a2cd40
Nova Ardiansyah <novaardiansyah78@gmail.com>
2023-01-15 15:44:41 +0700

    Improved helper and added functionality to save error logs from the server

--- application/views/Layout/Script.php
+++ application/views/Layout/Script.php
@@ -1,9 +1,22 @@
+<?php $this->load->library('user_agent'); ?>
+
 <script src="<?= adminlte_url('plugins/moment/moment-with-locales.min.js') ?>"></script>
 
 <script>
   const config = {
     'base_url': '<?= base_url() ?>',
     'adminlte_url': '<?= adminlte_url() ?>'
+  }
+
+  const userAgent = {
+    'user_agent': '<?= $this->agent->agent_string(); ?>',
+    'ip_address': '<?= $this->input->ip_address(); ?>',
+    'browser': '<?= $this->agent->browser(); ?>',
+    'browser_version': '<?= $this->agent->version(); ?>',
+    'platform': '<?= $this->agent->platform(); ?>',
+    'mobile': '<?= $this->agent->mobile(); ?>',
+    'robot': '<?= $this->agent->robot(); ?>',
+    'referrer': '<?= $this->agent->referrer(); ?>'
   }
 </script>
 
